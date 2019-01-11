53af14caf4fdd4455d1d054c141f50b63d493208
sumaiadarra <darra.it@hotmail.com>
2019-01-11 11:04:47 +0100

    changing Name of the App

--- app/src/main/java/com/example/user/myanotherapp/MainActivity.java
+++ app/src/main/java/com/example/user/myanotherapp/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
